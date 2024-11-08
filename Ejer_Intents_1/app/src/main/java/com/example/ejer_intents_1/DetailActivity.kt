8583053bf712bcf6e677d37f0e6b6947a96003d1
vnhrr <ivan.herrero.galvan@iesjulianmarias.es>
2024-11-08 15:32:17 +0100

    08-11-2024 4

--- Ejer_Intents_1/app/src/main/java/com/example/ejer_intents_1/DetailActivity.kt
+++ Ejer_Intents_1/app/src/main/java/com/example/ejer_intents_1/DetailActivity.kt
@@ -1,33 +1,17 @@
 package com.example.ejer_intents_1
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 
 class DetailActivity : AppCompatActivity()  {
 
-    var textViewDatos = findViewById<TextView>(R.id.textViewDetalles)
-
-    private val startForResult = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()) { result ->
-        // Este bloque de código se ejecutará cuando ProvinceActivity envíe un resultado de vuelta.
-        // Si el resultado es correcto
-        if (result.resultCode == Activity.RESULT_OK) {
-            // Obtenemos el dato "province" del Intent que ha vuelto.
-            val selectedProvince = result.data?.getStringExtra("province")
-            // Establecemos el texto del TextView con la provincia seleccionada.
-            textViewDatos.text = selectedProvince
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        var textViewDatos = findViewById<TextView>(R.id.textViewDetalles)
 
-        startForResult.launch(intent)
+        textViewDatos.text = intent.getStringExtra("datos")
     }
 }
--- Ejer_Intents_1/app/src/main/java/com/example/ejer_intents_1/MainActivity.kt
+++ Ejer_Intents_1/app/src/main/java/com/example/ejer_intents_1/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.ejer_intents_1
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,19 +20,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editTextNombre = findViewById<EditText>(R.id.editTextNombre)
-        var textViewEdad = findViewById<TextView>(R.id.textViewEdadReal)
+        val textViewEdad = findViewById<TextView>(R.id.textViewEdadReal)
         val seekbarEdad = findViewById<SeekBar>(R.id.seekBarEdad)
         var edad = ""
         seekbarEdad.min = 10
         seekbarEdad.max = 100
         val buttonEnviar = findViewById<Button>(R.id.buttonEnviar)
 
-        var nombre = editTextNombre.text
 
         seekbarEdad.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 textViewEdad.text = "$progress"
                 edad = progress.toString()
+                Log.d("edad", edad)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
@@ -39,9 +41,12 @@ class MainActivity : AppCompatActivity() {
         })
 
         buttonEnviar.setOnClickListener {
+            val nombre = editTextNombre.text
+
+            val datos = "Tu nombre es $nombre y tienes $edad years"
+            Log.d("edad", datos)
             val intent = Intent(this, DetailActivity::class.java)
-            intent.putExtra("datos", "Tu nombre es $nombre y tienes $edad years")
-            setResult(Activity.RESULT_OK, intent)
+            intent.putExtra("datos", datos)
             this.startActivity(intent)
         }
 
